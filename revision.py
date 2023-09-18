04112bfa02e87deb5062fe62adc2950a53e5f07c
Henry Li <lih8@tcd.ie>
2023-09-18 20:31:50 +0100

    Add turtle lesson

---
+++ revision.py
@@ -0,0 +1,14 @@
+# Strings "hello world"
+# Int -98
+# Float 3.4
+# List ["a", "b", "cv"]
+# Boolean True/False
+
+# firstName = input("Please enter your first name: ")
+# lastName = input("Please enter your last name: ")
+print("44" + 33)
+
+age = input("Please enter your age: ")
+print(age + 4)
+
+# print("Nice to meet you " + firstName + " " + lastName)
---
+++ turtleLearn.py
@@ -0,0 +1,30 @@
+import turtle as t
+
+# penWidth = 1
+t.color("blue")
+
+squareWidth = 200
+
+for i in range(100):
+    # penWidth = penWidth + 1
+    # t.width(penWidth)
+    t.forward(200)
+    t.left(90)
+
+t.mainloop()
+# left(120)
+# forward(100)
+# left(120)
+# forward(100)
+
+# for steps in range(100):
+#     for c in ('blue', 'red', 'green'):
+#         color(c)
+#         forward(steps)
+#         right(30)
+
+# while True:
+#     forward(200)
+#     left(170)
+#     if abs(pos()) < 1:
+#         break
