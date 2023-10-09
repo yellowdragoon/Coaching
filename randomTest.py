c40226e6bf633a5c5dbfa3f8c892073a3eeb85e6
Henry Li <lih8@tcd.ie>
2023-10-09 20:43:22 +0100

    Add turtle lesson next tasks

---
+++ randomTest.py
@@ -0,0 +1,6 @@
+import random
+
+test = [1, 2, 3, 4, 5, 6]
+
+for i in range(100):
+    print(random.choice(test))
--- turtle/project.py
+++ turtle/project.py
@@ -1,4 +1,5 @@
 import turtle as t
+import time
 import random
 
 """
@@ -12,17 +13,35 @@ Make a finish circle for each turtle/finish line.
 Make a die by creating a list of integers 1-6.
 Figure out how to use the random library in python to choose a random number from the list you have created.
 """
+die_numbers = [1, 2, 3, 4, 5, 6]
+
+t.color("red")
+t.shape("turtle")
 
 t.penup()
 t.goto(-400, 300)
+t.forward(750)
+t.pendown()
+t.dot(50)
+t.penup()
+t.back(750)
 
 t2 = t.clone()
 t2.goto(-400, 100)
-
-t.color("red")
 t2.color("blue")
+t2.forward(750)
+t2.pendown()
+t2.dot(50)
+t2.penup()
+t2.back(750)
 
-t.shape("turtle")
-t2.shape("turtle")
+while True:
+    random.choice(die_numbers)
+    # For turtle 1, pick a random number from the list
+    # Move that turtle forward by that number of steps
+    # Use the time library to wait 0.5 seconds
+    # Do the same for the second turtle
+
+
 
 t.mainloop()
